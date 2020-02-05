9faa1a2edf969d0a2e67dc22d37ef24572f6faa6
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2020-02-05 14:22:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 $(document).ready(function () {
+  $('.animate-fade-in').each( function(i){
+    var fadeLocation = $(this).offset().top + 0.25*($(window).height());
+    var windowBottom = $(window).scrollTop() + $(window).height();
+    /* If the object is visible in the window, fade in */
+    if( windowBottom > fadeLocation ){
+      $(this).delay(150).animate({'opacity':'1'},600);
+    }
+  });
 });
