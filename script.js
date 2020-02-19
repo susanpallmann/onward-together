2e109f24ff3d2412cae8f2038641c07bdcaf954e
susanpallmann <38414619+susanpallmann@users.noreply.github.com>
2020-02-18 21:36:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -129,10 +129,10 @@ function gatherIds () {
 function updateCounters(item, index) {
   dbLocation = item;
   console.log(dbLocation);
-  var pathRef = firebase.database().ref('counters/' + dbLocation);
-  var countRef = firebase.database().ref('counters/' + dbLocation + '/count');
+  var pathRef = firebase.database().ref('counters/' + 11111);
+  var countRef = firebase.database().ref('counters/' + 11111 + '/count');
   var anotherCount;
-  var loadCount = pathRef.once('value').then(function(snapshot) {
+  var loadCount = countRef.once('value').then(function(snapshot) {
     console.log(snapshot.val());
   });
   //var existingCount = parseInt(anotherCount);
